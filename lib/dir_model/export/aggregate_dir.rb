710c8927e852366737f13aeb4fa4c982a02cddc4
Joel AZEMAR <joel.azemar@gmail.com>
2015-11-05 11:02:30 +0100

    call modules

--- lib/dir_model/export/aggregate_dir.rb
+++ lib/dir_model/export/aggregate_dir.rb
@@ -1,25 +1,29 @@
 require 'active_support/hash_with_indifferent_access'
 
-class DirModel::Export::AggregateDir
+module DirModel
+  module Export
+    class AggregateDir
 
-  attr_reader :export_dir_model_class, :context, :paths
+      attr_reader :export_dir_model_class, :context, :paths
 
-  # @param [Export] export_model export model class
-  def initialize(export_dir_model_class, context={})
-    @export_dir_model_class = export_dir_model_class
-    @context = context.to_h #.symbolize_keys
-  end
+      # @param [Export] export_model export model class
+      def initialize(export_dir_model_class, context={})
+        @export_dir_model_class = export_dir_model_class
+        @context = context.to_h #.symbolize_keys
+      end
 
-  # Add a row_model to the
-  # @param [] source_model the source model of the export file model
-  # @param [Hash] context the extra context given to the instance of the file model
-  def append_model(source_model, context={})
-    @paths << export_dir_model_class.new(source_model, context.to_h.reverse_merge(self.context)).path
-  end
-  alias_method :<<, :append_model
+      # Add a row_model to the
+      # @param [] source_model the source model of the export file model
+      # @param [Hash] context the extra context given to the instance of the file model
+      def append_model(source_model, context={})
+        @paths << export_dir_model_class.new(source_model, context.reverse_merge(self.context)).path
+      end
+      alias_method :<<, :append_model
 
-  def generate
-    @paths ||= []
-    yield self
+      def generate
+        @paths ||= []
+        yield self
+      end
+    end
   end
 end
