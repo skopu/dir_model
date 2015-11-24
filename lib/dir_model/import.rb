378dd77839d44dba269a12ced1125a46571310c6
Joel AZEMAR <joel.azemar@gmail.com>
2015-11-24 18:43:15 +0100

    change caching strategy

--- lib/dir_model/import.rb
+++ lib/dir_model/import.rb
@@ -2,35 +2,55 @@ module DirModel
   module Import
     extend ActiveSupport::Concern
 
-    attr_reader :context, :source_path, :index, :previous, :match
+    attr_reader :context, :source_path, :index, :previous
 
     def initialize(path, options={})
       @source_path, @context = path, OpenStruct.new(options[:context])
       @index, @previous = options[:index], options[:previous].try(:dup)
+      @load_state = :ghost
+    end
+
+    def matches
+      load
+      @match
     end
 
     def skip?
+      load
       !@_match
     end
 
     module ClassMethods
       def next(path, context={}, previous=nil)
         path.read_path
-        new(path.current_path, index: path.index, context: context, previous: previous).tap do |import_model|
-          import_model.send(:match?)
-        end
+        new(path.current_path, index: path.index, context: context, previous: previous)
       end
     end
 
     private
 
+    attr_reader :load_state
+
     def match?
+      return if load_state == :loaded
+
       @_match ||= begin
-        self.class.files.each do |file, attributes|
-          return true if (@match = (source_path||'').match(attributes[:regex].call))
+        loader do
+          self.class.files.each do |file, attributes|
+            return true if (@match = (source_path||'').match(attributes[:regex].call))
+          end
+          false
         end
-        false
       end
     end
+    alias_method :load, :match?
+
+    def loader
+      @load_state = :loading
+      result = yield
+      @load_state = :loaded
+      result
+    end
+
   end
 end
